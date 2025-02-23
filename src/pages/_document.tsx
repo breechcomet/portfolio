35baa8dbfb4aa13a8fae28bd9b786143329dd507
breechcomet <munashemoyo27@gmail.com>
2025-02-23 19:48:28 +0200

    Fifth Commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Munashe Moyo's Portfolio" />
+        <meta property="og:description" content="Welcome to my portfolio. Here you will find my journal entries as well as some stuff I have worked on and am currently working on." />
+        <meta property="og:image" content="https://i.ibb.co/VW9MMsxQ/Munashe-Avatar.jpg" />
+        <meta property="og:url" content="https://munashe.top" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
